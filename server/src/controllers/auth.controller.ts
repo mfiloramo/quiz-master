45aab6126acb1d0a1ae2990498f371a28eb1a644
Michael Filoramo <mlfiloramo@gmail.com>
2024-12-24 17:00:03 -0500

    Build basic AuthController and modify router

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -2,10 +2,31 @@ import { Request, Response } from 'express';
 import axios from 'axios';
 
 
-export const authController = async (req: Request, res: Response): Promise<void> => {
-  try {
-    res.status(200).send('Controller response successful!');
-  } catch (error) {
-    res.status(500).send('Internal server error');
+export class AuthController {
+  static async register(req: Request, res: Response): Promise<void> {
+    try {
+      res.status(200).send('AuthController register successful!');
+    } catch (error) {
+      res.status(500).send('Internal server error');
+    }
   }
-};
+
+  static async login(req: Request, res: Response): Promise<void> {
+    try {
+      res.status(200).send('AuthController login successful!');
+    } catch (error) {
+      res.status(500).send('Internal server error');
+    }
+  }
+
+  static async logout(req: Request, res:
+    Response): Promise<void> {
+    try {
+      res.status(200).send('AuthController logout successful!');
+    } catch (error) {
+      res.status(500).send('Internal server error');
+    }
+  }
+}
+
+
--- server/src/routers/auth.router.ts
+++ server/src/routers/auth.router.ts
@@ -1,12 +1,12 @@
 import express, { Router } from 'express';
-import { authController } from "../controllers/auth.controller";
+import { AuthController } from '../controllers/auth.controller'
 
 
 const router: Router = express.Router();
 
-router.post('/register', authController);
-router.post('/login', authController);
-router.post('/logout', authController);
+router.post('/register', AuthController.register);
+router.post('/login', AuthController.login);
+router.post('/logout', AuthController.logout);
 
 
 export const authRouter: Router = router;
