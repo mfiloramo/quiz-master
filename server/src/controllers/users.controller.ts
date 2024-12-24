80d6fdcdb0d363437f6626aa541df58d33cdff61
Michael Filoramo <mlfiloramo@gmail.com>
2024-12-24 17:04:39 -0500

    Build basic UsersController and modify router

--- server/src/controllers/users.controller.ts
+++ server/src/controllers/users.controller.ts
@@ -2,10 +2,40 @@ import { Request, Response } from 'express';
 import axios from 'axios';
 
 
-export const usersController = async (req: Request, res: Response): Promise<void> => {
-  try {
-    res.status(200).send('Controller response successful!');
-  } catch (error) {
-    res.status(500).send('Internal server error');
+export class UsersController {
+  static async register(req: Request, res: Response): Promise<void> {
+    try {
+      res.status(200).send('AuthController register successful!');
+    } catch (error) {
+      res.status(500).send('Internal server error');
+    }
   }
-};
+
+  static async getUserById(req: Request, res: Response): Promise<void> {
+    try {
+      res.status(200).send('UserController getUserById successful!');
+    } catch (error) {
+      res.status(500).send('Internal server error');
+    }
+  }
+
+  static async updateUser(req: Request, res:
+    Response): Promise<void> {
+    try {
+      res.status(200).send('UserController updateUser successful!');
+    } catch (error) {
+      res.status(500).send('Internal server error');
+    }
+  }
+
+  static async deleteUser(req: Request, res:
+    Response): Promise<void> {
+    try {
+      res.status(200).send('UserController deleteUser successful!');
+    } catch (error) {
+      res.status(500).send('Internal server error');
+    }
+  }
+}
+
+
--- server/src/routers/users.router.ts
+++ server/src/routers/users.router.ts
@@ -1,15 +1,12 @@
 import express, { Router } from 'express';
-import { usersController } from "../controllers/users.controller";
+import { UsersController } from "../controllers/users.controller";
 
 
 const router: Router = express.Router();
 
-router.get('/', usersController);
-router.post('/auth/register', usersController);
-router.post('/auth/login', usersController);
-router.get('/:id', usersController);
-router.put('/:id', usersController);
-router.delete('/:id', usersController);
+router.get('/:id', UsersController.getUserById);
+router.put('/:id', UsersController.updateUser);
+router.delete('/:id', UsersController.deleteUser);
 
 
 export const usersRouter: Router = router;
