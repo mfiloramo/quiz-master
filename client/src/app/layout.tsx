ed79b8ea3f1313ffd8deb0e1dc8e3f445bb0654a
Michael Filoramo <mlfiloramo@gmail.com>
2024-12-28 20:19:20 -0500

    Add favicon; Add Dashboard page placeholder

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import type { Metadata } from "next";
 import { NextFont } from "next/dist/compiled/@next/font";
 import "./globals.css";
-import { Bitter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import Navbar from "@/components/navbar/navbar";
 
-const bitter: NextFont = Bitter({ subsets: [ 'latin' ]});
+const poppins_light: NextFont = Poppins({ weight: "200" });
+const poppins_heavy: NextFont = Poppins({ weight: "400" });
 
 export const metadata: Metadata = {
   title: "QuizMaster",
@@ -16,7 +17,7 @@ export default function RootLayout({ children }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={ bitter.className } suppressHydrationWarning>
+    <html lang="en" className={ poppins_heavy.className } suppressHydrationWarning>
     <body className="antialiased bg-white">
     <Navbar/>
     { children }
