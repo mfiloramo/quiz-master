aeefd599145c75a2117ab1ebd52f30ae12a5d930
Michael Filoramo <mlfiloramo@gmail.com>
2025-04-17 21:50:22 -0400

    Reformat controllers; Change questions route

--- server/src/controllers/question.controller.ts
+++ server/src/controllers/question.controller.ts
@@ -1,31 +1,32 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 import { sequelize } from "../config/sequelize";
-
 
 export class QuestionController {
   // ADD NEW QUESTION TO DATABASE
   static async addQuestion(req: Request, res: Response): Promise<void> {
     try {
       const { quizId, question, options, correct } = req.body;
-      await sequelize.query('EXECUTE AddQuestion :quizId, :question, :options, :correct',
+      await sequelize.query(
+        "EXECUTE AddQuestion :quizId, :question, :options, :correct",
         {
-          replacements: { quizId, question, options, correct }
-        });
-      res.status(200).send(`Question added to Quiz ID: ${ quizId }.`);
+          replacements: { quizId, question, options, correct },
+        },
+      );
+      res.status(200).send(`Question added to Quiz ID: ${quizId}.`);
     } catch (error: any) {
-      console.error('Error executing Stored Procedure:', error.message);
-      res.status(500).send('Internal server error');
+      console.error("Error executing Stored Procedure:", error.message);
+      res.status(500).send("Internal server error");
     }
   }
 
   // GET ALL QUESTIONS FROM DATABASE
   static async getAllQuestions(req: Request, res: Response): Promise<void> {
     try {
-      const questions: any[] = await sequelize.query('EXECUTE GetAllQuestions');
+      const questions: any[] = await sequelize.query("EXECUTE GetAllQuestions");
       res.send(questions[0]);
     } catch (error: any) {
-      console.error('Error executing Stored Procedure:', error.message);
-      res.status(500).send('Internal server error');
+      console.error("Error executing Stored Procedure:", error.message);
+      res.status(500).send("Internal server error");
     }
   }
 
@@ -33,29 +34,36 @@ export class QuestionController {
   static async getQuestionById(req: Request, res: Response): Promise<void> {
     try {
       const { questionId } = req.params;
-      const question: any = await sequelize.query('EXECUTE GetQuestionById :questionId',
+      const question: any = await sequelize.query(
+        "EXECUTE GetQuestionById :questionId",
         {
-          replacements: { questionId }
-        });
+          replacements: { questionId },
+        },
+      );
       res.send(question[0][0]);
     } catch (error: any) {
-      console.error('Error executing Stored Procedure:', error.message);
-      res.status(500).send('Internal server error');
+      console.error("Error executing Stored Procedure:", error.message);
+      res.status(500).send("Internal server error");
     }
   }
 
   // GET ALL QUESTIONS BY QUIZ ID
-  static async getQuestionsByQuizId(req: Request, res: Response): Promise<void> {
+  static async getQuestionsByQuizId(
+    req: Request,
+    res: Response,
+  ): Promise<void> {
     try {
-      const { questionId, quizId } = req.params;
-      const questions: any[] = await sequelize.query('EXECUTE GetQuestionsByQuizId :quizId',
+      const { quizId } = req.params;
+      const questions: any[] = await sequelize.query(
+        "EXECUTE GetQuestionsByQuizId :quizId",
         {
-          replacements: { quizId }
-        });
+          replacements: { quizId },
+        },
+      );
       res.send(questions[0]);
     } catch (error: any) {
-      console.error('Error executing Stored Procedure:', error.message);
-      res.status(500).send('Internal server error');
+      console.error("Error executing Stored Procedure:", error.message);
+      res.status(500).send("Internal server error");
     }
   }
 
@@ -63,29 +71,34 @@ export class QuestionController {
   static async updateQuestion(req: Request, res: Response): Promise<void> {
     try {
       const { questionId, question, options, correct } = req.body;
-      await sequelize.query('EXECUTE UpdateQuestion :questionId, :question, :options, :correct',
+      await sequelize.query(
+        "EXECUTE UpdateQuestion :questionId, :question, :options, :correct",
         {
-          replacements: { questionId, question, options, correct }
-        })
-      res.status(200).send(`Question with ID: ${ questionId } updated successfully`);
+          replacements: { questionId, question, options, correct },
+        },
+      );
+      res
+        .status(200)
+        .send(`Question with ID: ${questionId} updated successfully`);
     } catch (error: any) {
-      console.error('Error executing Stored Procedure:', error.message);
-      res.status(500).send('Internal server error');
+      console.error("Error executing Stored Procedure:", error.message);
+      res.status(500).send("Internal server error");
     }
   }
 
   // DELETE EXISTING QUESTION
   static async deleteQuestion(req: Request, res: Response): Promise<void> {
     try {
-      const { questionId }  = req.body;
-      await sequelize.query('EXECUTE DeleteQuestion :questionId',
-        {
-          replacements: { questionId }
-        });
-      res.status(200).send(`Question with ID: ${ questionId } deleted successfully`);
+      const { questionId } = req.body;
+      await sequelize.query("EXECUTE DeleteQuestion :questionId", {
+        replacements: { questionId },
+      });
+      res
+        .status(200)
+        .send(`Question with ID: ${questionId} deleted successfully`);
     } catch (error: any) {
-      console.error('Error executing Stored Procedure:', error.message);
-      res.status(500).send('Internal server error');
+      console.error("Error executing Stored Procedure:", error.message);
+      res.status(500).send("Internal server error");
     }
   }
 }
--- server/src/routers/question.router.ts
+++ server/src/routers/question.router.ts
@@ -1,19 +1,16 @@
-import express, { Router } from 'express';
+import express, { Router } from "express";
 import { QuestionController } from "../controllers/question.controller";
-
 
 const router: Router = express.Router();
 
 // PROTECT ALL ROUTES WITH JWT VERIFICATION MIDDLEWARE
 // router.use(authenticateJWT);
 
-router.post('/:questionId', QuestionController.addQuestion);
-router.get('/', QuestionController.getAllQuestions);
-router.get('/:questionId', QuestionController.getQuestionById);
-// TODO: MAKE ROUTE MORE INTUITIVE; ONE PARAM NOT USED IN CONTROLLER; CONSIDER ADDING LONGER PATH
-router.get('/:quizId/:questionId/', QuestionController.getQuestionsByQuizId);
-router.put('/:questionId', QuestionController.updateQuestion);
-router.delete('/:questionId', QuestionController.deleteQuestion);
-
+router.post("/:questionId", QuestionController.addQuestion);
+router.get("/", QuestionController.getAllQuestions);
+router.get("/:questionId", QuestionController.getQuestionById);
+router.get("/quiz/:quizId/", QuestionController.getQuestionsByQuizId);
+router.put("/:questionId", QuestionController.updateQuestion);
+router.delete("/:questionId", QuestionController.deleteQuestion);
 
 export const questionRouter: Router = router;
