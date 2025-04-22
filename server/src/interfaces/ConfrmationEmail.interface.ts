8303313244c50a624eb7fe6d752f134ee629340f
Michael Filoramo <mlfiloramo@gmail.com>
2025-04-22 12:18:32 -0400

    Add ConfirmationEmail.interface

---
+++ server/src/interfaces/ConfrmationEmail.interface.ts
@@ -0,0 +1,6 @@
+export interface ConfirmationEmailInterface {
+  readonly userId: string,
+  readonly userName: string,
+  readonly userEmail: string
+
+}
--- server/src/services/EmailService.ts
+++ server/src/services/EmailService.ts
@@ -1,12 +1,9 @@
 import { CourierClient } from "@trycourier/courier";
 import jwt from "jsonwebtoken";
+import { ConfirmationEmailInterface } from "../interfaces/ConfrmationEmail.interface";
 
 export class EmailService {
-  static async sendConfirmationEmail({ userId, userName, userEmail }: {
-    userId: string,
-    userName: string,
-    userEmail: string
-  }) {
+  static async sendConfirmationEmail({ userId, userName, userEmail }: ConfirmationEmailInterface): Promise<boolean> {
 
     if (!process.env.SECRET_REGISTRATION_KEY) {
       throw new Error("SECRET_REGISTRATION_KEY is not set");
@@ -16,9 +13,11 @@ export class EmailService {
       throw new Error("COURIER_AUTH_TOKEN is not set");
     }
 
+    // SIGN JWT AND GENERATE ACCOUNT ACTIVATION LINK
     const token = jwt.sign({ userId }, process.env.SECRET_REGISTRATION_KEY, { expiresIn: "1d" });
     const activateAccountLink = `${process.env.API_BASE_URL}/auth/activate/${token}`;
 
+    // SEND CONFIRMATION EMAIL TO USER
     const courier = new CourierClient({ authorizationToken: process.env.COURIER_AUTH_TOKEN });
 
     await courier.send({
