1e3a67b030816816c43071e19855fd058e41b56d
Michael Filoramo <mlfiloramo@gmail.com>
2025-04-23 21:03:01 -0400

    Refactor form handling logic for Quiz create form

--- client/src/app/dashboard/create/page.tsx
+++ client/src/app/dashboard/create/page.tsx
@@ -6,8 +6,9 @@ import { useAuth } from '@/contexts/AuthContext';
 
 export default function Create(): ReactElement {
   // STATE HOOKS
-  const [title, setTitle] = useState<string>('');
-  const [description, setDescription] = useState<string>('');
+  const [form, setForm] = useState<any>({ title: '', description: '' });
+  // const [title, setTitle] = useState<string>('');
+  // const [description, setDescription] = useState<string>('');
 
   // ROUTER INSTANCE FOR REDIRECTION
   const router = useRouter();
@@ -26,22 +27,30 @@ export default function Create(): ReactElement {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ userId: user!.id, username: user!.username, title, description }),
+        body: JSON.stringify({
+          userId: user!.id,
+          username: user!.username,
+          title: form.title,
+          description: form.description,
+        }),
       });
 
       const data: any = await response.json();
-      console.log(data);
 
       // HANDLE INVALID RESPONSE
       if (!response.ok) {
         throw new Error('Invalid email or password');
-      } else if (response.ok) {
-        // ROUTE USER TO EDIT PAGE IF RESPONSE IS OK
-        router.push('/dashboard/edit');
       }
+
+      router.push('/dashboard/edit');
     } catch (error: any) {
       console.error(error);
     }
+  };
+
+  // HANDLE FORM CHANGES
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
   };
 
   return (
@@ -65,38 +74,44 @@ export default function Create(): ReactElement {
         <div className={'mx-4 flex w-full flex-col items-start rounded-lg bg-slate-300 p-3'}>
           {/* TITLE SECTION */}
           <div className={'w-full text-2xl font-bold'}>
-            Title (required)
-            <form className={'my-2'}>
-              <input
-                type={'text'}
-                placeholder={'Give your quiz a cool title...'}
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                className={'w-full rounded p-3'}
-              />
-            </form>
-            {/* DESCRIPTION SECTION */}
-            <div className={'mt-5 w-full text-2xl font-bold'}>
-              Description
-              <form className={'my-2'}>
+            <form className={'my-2'} onSubmit={handleSubmit}>
+              {/* TITLE INPUT */}
+              <div className={'w-full text-2xl font-bold'}>
+                Title (required)
                 <input
+                  name={'title'}
+                  type={'text'}
+                  placeholder={'Give your quiz a cool title...'}
+                  value={form.title}
+                  onChange={handleChange}
+                  className={'w-full rounded p-3'}
+                  required
+                />
+              </div>
+
+              {/* DESCRIPTION INPUT */}
+              <div className={'mt-5 w-full text-2xl font-bold'}>
+                Description
+                <input
+                  name={'description'}
                   type={'text'}
                   placeholder={'Tell us what your quiz will be about...'}
-                  value={description}
-                  onChange={(e) => setDescription(e.target.value)}
+                  value={form.description}
+                  onChange={handleChange}
                   className={'h-[8vh] w-full rounded p-3'}
                 />
-              </form>
-            </div>
-            {/* SUBMIT BUTTON */}
-            <button
-              className={
-                'h-12 w-24 cursor-pointer rounded-lg bg-cyan-600 text-xl font-medium text-cyan-200 shadow transition hover:bg-cyan-500 active:bg-cyan-400'
-              }
-              onClick={handleSubmit}
-            >
-              Submit
-            </button>
+              </div>
+
+              {/* SUBMIT BUTTON */}
+              <button
+                type='submit'
+                className={
+                  'mt-3 h-12 w-24 cursor-pointer rounded-lg bg-cyan-600 text-xl font-medium text-cyan-200 shadow transition hover:bg-cyan-500 active:bg-cyan-400'
+                }
+              >
+                Submit
+              </button>
+            </form>
           </div>
         </div>
       </div>
