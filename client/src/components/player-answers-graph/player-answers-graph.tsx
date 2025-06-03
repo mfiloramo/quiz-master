b4b9cc36677a861f8035d3e7339060af9cd1855d
Michael Filoramo <mlfiloramo@gmail.com>
2025-06-03 19:54:31 -0400

    Remove console.log in PlayerAnswerGraph

--- client/src/components/player-answers-graph/player-answers-graph.tsx
+++ client/src/components/player-answers-graph/player-answers-graph.tsx
@@ -23,8 +23,6 @@ export default function PlayerAnswersGraph({
   playerAnswers,
   options,
 }: Props): ReactElement | undefined {
-  console.log('playerAnswers', playerAnswers);
-
   // BUILD THE DATA ARRAY BASED ON PLAYER ANSWERS
   const data = useMemo(() => {
     // PLAYER ANSWERS IS UNDEFINED
