21171925bd645e1f972a36d195313ddfea38b1f2
Michael Filoramo <mlfiloramo@gmail.com>
2025-06-05 12:57:42 -0400

    Fix bug: FinalScoreboard crashing with fewer than 3 players

--- client/src/components/final-scoreboard/final-scoreboard.tsx
+++ client/src/components/final-scoreboard/final-scoreboard.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { ReactElement } from 'react';
+import React, { JSX, ReactElement } from 'react';
 import { motion } from 'framer-motion';
 import { useSession } from '@/contexts/SessionContext';
 import { Player } from '@/interfaces/PlayerListProps.interface';
@@ -11,7 +11,7 @@ const barHeights = ['70%', '100%', '40%'];
 const barColors = ['bg-amber-500', 'bg-green-600', 'bg-red-500'];
 const delays = [1.0, 1.5, 0.5];
 
-export default function FinalScoreboard(): ReactElement {
+export default function FinalScoreboard(): JSX.Element {
   // CUSTOM HOOKS
   const { players } = useSession();
 
@@ -31,24 +31,27 @@ export default function FinalScoreboard(): ReactElement {
 
       {/* INNER CONTAINER */}
       <div className='flex h-[40vh] w-fit max-w-2xl items-end justify-between gap-6 rounded bg-slate-400 px-8 py-4 shadow-xl'>
-        {displayOrder.map((i: number) => (
-          <motion.div
-            key={i}
-            initial={{ height: '0%' }}
-            animate={{ height: barHeights[displayOrder.indexOf(i)] }}
-            transition={{
-              duration: 0.8,
-              delay: delays[displayOrder.indexOf(i)],
-              ease: [0, 0.71, 0.2, 1.01],
-            }}
-            className={`flex w-32 min-w-24 max-w-36 flex-col items-center justify-end rounded-xl ${barColors[displayOrder.indexOf(i)]} text-white shadow-xl`}
-          >
-            <div className='p-2 text-center'>
-              <div className='text-lg font-semibold'>{playerFinalists[i].username}</div>
-              <div className='text-sm'>Score: {playerFinalists[i].score}</div>
-            </div>
-          </motion.div>
-        ))}
+        {displayOrder.map(
+          (i: number) =>
+            playerFinalists[i] && (
+              <motion.div
+                key={i}
+                initial={{ height: '0%' }}
+                animate={{ height: barHeights[displayOrder.indexOf(i)] }}
+                transition={{
+                  duration: 0.8,
+                  delay: delays[displayOrder.indexOf(i)],
+                  ease: [0, 0.71, 0.2, 1.01],
+                }}
+                className={`flex w-32 min-w-24 max-w-36 flex-col items-center justify-end rounded-xl ${barColors[displayOrder.indexOf(i)]} text-white shadow-xl`}
+              >
+                <div className='p-2 text-center'>
+                  <div className='text-lg font-semibold'>{playerFinalists[i].username}</div>
+                  <div className='text-sm'>Score: {playerFinalists[i].score}</div>
+                </div>
+              </motion.div>
+            )
+        )}
       </div>
     </div>
   );
