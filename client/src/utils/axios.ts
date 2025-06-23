b07699d5f147a95cd3ba3da9386d64938a0c84fe
Michael Filoramo <mlfiloramo@gmail.com>
2025-06-22 23:09:46 -0400

    Normalize session join code casing; Add console log to joinSession (debug)

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -7,7 +7,7 @@ import axios from 'axios';
 const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3030/api',
+  baseURL: baseURL,
   withCredentials: true, // REQUIRED if server uses `credentials: true` in CORS
 });
 
