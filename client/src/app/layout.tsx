187fd358f69b672720fdc2fe907d85ad4c2380b4
Michael Filoramo <mlfiloramo@gmail.com>
2025-08-06 08:32:40 -0400

    Overscroll testing

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en' className={poppins.className}>
-      <body className='bg-white antialiased'>
+      <body className='flex-1 overflow-y-auto overscroll-contain bg-white antialiased'>
         <AuthProvider>
           <ScrollToTop />
           {/* NAVBAR */}
