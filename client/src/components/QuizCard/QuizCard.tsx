225a23c27ebecad1f54371900e4bd7bf61623afe
Michael Filoramo <mlfiloramo@gmail.com>
2025-08-12 12:44:09 -0400

    Refactor quiz delete button in Library page

--- client/src/components/QuizCard/QuizCard.tsx
+++ client/src/components/QuizCard/QuizCard.tsx
@@ -1,34 +1,14 @@
 'use client';
 
 import React, { ReactElement } from 'react';
-import { Trash2 } from 'lucide-react';
 import { QuizCardProps } from '@/types/Quiz.types';
-import { usePathname } from 'next/navigation';
 import { motion } from 'framer-motion';
-import axiosInstance from '@/utils/axios';
 
-export default function MainQuizCard({
-  quiz,
-  selected,
-  onSelect,
-  onDelete,
-}: QuizCardProps): ReactElement {
-  const pathname: string = usePathname();
-
-  // DELETE SELECTED QUIZ
-  const handleDelete = async (e: React.MouseEvent) => {
-    e.stopPropagation(); // PREVENT onSelect WHEN CLICKING DELETE
-
-    try {
-      onDelete!(quiz.id); // TRIGGER PARENT STATE UPDATE
-    } catch (err) {
-      console.error('Failed to delete quiz:', err);
-    }
-  };
-
+export default function MainQuizCard({ quiz, selected, onSelect }: QuizCardProps): ReactElement {
   return (
     // QUIZ CARD CONTAINER
     <motion.div
+      key={quiz.id}
       whileHover={{ scale: 1.03 }}
       whileTap={{ scale: 0.97 }}
       transition={{ duration: 0.001 }}
@@ -37,14 +17,9 @@ export default function MainQuizCard({
         selected ? 'bg-sky-300/75' : 'bg-white/75 hover:bg-cyan-100/75'
       }`}
     >
-      {/* HEADER (TITLE + ICONS) */}
+      {/* HEADER (TITLE) */}
       <div className='mb-2 flex w-full items-center justify-between'>
         <span className='font-semibold'>{quiz.title}</span>
-        <div className='flex gap-2'>
-          {!pathname.includes('discover') ? (
-            <Trash2 className='cursor-pointer text-red-500' onClick={handleDelete} />
-          ) : null}
-        </div>
       </div>
 
       {/* DESCRIPTION */}
